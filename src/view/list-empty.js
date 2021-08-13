a2b2b2338598c95d64a6bf23a9bafaa9f5134f7c
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2021-08-13 11:39:04 +0300

    Реализовано скрытие попапа по esc и добавлен компонент, показывающий сообщение при отсутсвие фильмов в базе данных

---
+++ src/view/list-empty.js
@@ -0,0 +1,29 @@
+import {createElement} from '../utils.js';
+
+const createListEmptyTemplate = () => (
+  `<section class="films-list">
+    <h2 class="films-list__title">There are no movies in our database</h2>
+  </section>`
+);
+
+export default class listEmpty {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createListEmptyTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
